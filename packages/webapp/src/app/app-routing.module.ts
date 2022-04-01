9fdd5331823bd12a962afc8a2dabe75ffad80f78
groths89-at-487435587416 <11449927+groths89@users.noreply.github.com>
2022-04-01 12:33:48 -0400

    add angular architects to container

--- packages/webapp/src/app/app-routing.module.ts
+++ packages/webapp/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full',
+    pathMatch: 'full'
   }
 ];
 
