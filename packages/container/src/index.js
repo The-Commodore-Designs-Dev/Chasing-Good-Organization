a485174ca5b84e8760892f4203dab6b28efa1276
groths89-at-487435587416 <11449927+groths89@users.noreply.github.com>
2022-04-01 13:26:33 -0400

    remove module type from index.js container

--- packages/container/src/index.js
+++ packages/container/src/index.js
@@ -1,7 +1,7 @@
 import { loadRemoteEntry } from '@angular-architects/module-federation';
 
 Promise.all([
-    loadRemoteEntry({type:'module', remoteEntry:'https://d2ldygdqblohqn.cloudfront.net/remoteEntry.js'})
+    loadRemoteEntry({remoteEntry:'https://d2ldygdqblohqn.cloudfront.net/remoteEntry.js'})
 ])
 .catch(err => console.error('Error loading remote entries', err))
 .then(() => import('./bootstrap.js'))
