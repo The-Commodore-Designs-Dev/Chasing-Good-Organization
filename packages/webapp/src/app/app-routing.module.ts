696208cfb22db8a218ea4e13c475cbac18470aae
groths89-at-487435587416 <11449927+groths89@users.noreply.github.com>
2022-04-08 11:26:01 -0400

    fix onsameurlnavigation

--- packages/webapp/src/app/app-routing.module.ts
+++ packages/webapp/src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     scrollPositionRestoration: 'top',
-    onSameNavigationReload: 'reload'
+    onSameUrlNavigation: 'reload'
   })],
   exports: [RouterModule]
 })
