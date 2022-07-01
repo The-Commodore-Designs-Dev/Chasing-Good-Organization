1d137012fa254ba8ec62d5846522e236849ccaa1
groths89-at-487435587416 <11449927+groths89@users.noreply.github.com>
2022-07-01 16:50:06 -0400

    Add newer version of @amplify/api

--- packages/webapp/src/app/API.service.ts
+++ packages/webapp/src/app/API.service.ts
@@ -3,7 +3,7 @@
 //  This file was automatically generated and should not be edited.
 import { Injectable } from "@angular/core";
 import API, { graphqlOperation, GraphQLResult } from "@aws-amplify/api-graphql";
-import { Observable } from "rxjs";
+import { Observable } from "zen-observable-ts";
 
 export interface SubscriptionResponse<T> {
   value: GraphQLResult<T>;
