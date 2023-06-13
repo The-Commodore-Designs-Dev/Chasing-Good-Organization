bd7c8c2e18d33fdce1be898b94d5e86de7622928
groths89-at-487435587416 <11449927+groths89@users.noreply.github.com>
2023-06-13 11:39:43 -0400

    delete some packages and debugging the deploy to production

--- packages/container/src/index.js
+++
@@ -1,8 +0,0 @@
-import { loadRemoteEntry } from '@angular-architects/module-federation';
-
-Promise.all([
-    loadRemoteEntry({remoteEntry:'https://d2ldygdqblohqn.cloudfront.net/remoteEntry.js'})
-])
-.catch(err => console.error('Error loading remote entries', err))
-.then(() => import('./bootstrap.js'))
-.catch(err => console.error(err));
